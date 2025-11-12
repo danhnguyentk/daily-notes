23bde6bebcf0bcde120afc4a7203eb7ff1510a93
Danh Nguyen <danh.nguyen@asnet.com.vn>
2025-11-12 17:33:36 +0700

    Remove multiple chart img keys

--- src/tradingviewService.ts
+++ src/tradingviewService.ts
@@ -25,38 +25,8 @@ export enum TradingviewInterval {
   Monthly = '1M',
 };
 
-// Because the free plan allows only 50 requests/day per key,
-// we use multiple Chart-Img API keys to distribute load.
-// Later, we can upgrade to a paid plan for unlimited usage.
-const CHART_IMAGE_KEYS = [
-  'fSaD8OZjPf8duW7BBdRpv2CkWn9TNwh77zh3N0FA',
-  'QMjMmjk1STaJOOWtfMpj48NUT1IWoJlD52mFmHFf',
-  'CGyeQHWjlu8A4Ho4G3yBXa0XuEyN4oXJ4AVZ1SLQ',
-  'xWJeAq6Dji1CRNCO4zYJkswW03X1jZh30JTbwEFc',
-  'vp0p0XhyD01OVFogsz9HC94JNreQghBm7hRyjS26',
-  'cv8D1LRZ799gZOfa18eNl9B1q4d0Qs6G2NnqXujz',
-  'YnGCnOh7cs2IAsHwiglEU9HDEGT6PIGH5AAE17rq',
-  'HvQKqwISnW1KkOQ1OiCMd5AkdBhwDH3N30jRJdzJ',
-  'AyJg91gR8a4JbRI9PxNPo67hgNKJm41w8s1Wgceu'
-];
-
-/**
- * Pick a random Chart-Img API key from the list.
- */
-function getRandomChartImageKey(): string {
-  if (CHART_IMAGE_KEYS.length === 0) {
-    throw new Error("No Chart-Img API keys configured!");
-  }
-  const randomIndex = Math.floor(Math.random() * CHART_IMAGE_KEYS.length);
-  return CHART_IMAGE_KEYS[randomIndex];
-}
-
 export async function getTradingViewImage(request: TradingviewRequest, env: Env): Promise<ArrayBuffer> {
   console.log(`Getting TradingView chart image for ${request.symbol} at interval ${request.interval}`);
-  // Use a random API key for each request to distribute load
-  // Will remove this once we upgrade to a paid plan
-  env.CHART_IMAGE_KEY = getRandomChartImageKey();
-  
   const url = `https://api.chart-img.com/v2/tradingview/layout-chart/${env.TRADINGVIEW_LAYOUT_ID}`;
   const response = await fetch(url, {
     method: 'POST',
